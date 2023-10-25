d22ec1bcec99c3fe853e44d6079f26e46e2783ed
Alejandro Leonian <alejandromleonian@gmail.com>
2023-10-25 11:10:51 -0500

    my version of naiveStringSearch is a bit more complicated than the teacher's

--- search/naiveStringSearch.js
+++ search/naiveStringSearch.js
@@ -1,19 +1,34 @@
+function naiveSearch(long, short) {
+    var count = 0;
+    for (var i = 0; i < long.length; i++) {
+        for (var j = 0; j < short.length; j++) {
+            if (short[j] !== long[i + j]) break;
+            if (j === short.length - 1) count++;
+        }
+    }
+    return count;
+}
+
+
 function naiveStringSearch(string, target) {
     let occurrences = 0;
     for (let i = 0; i < string.length; i++) {
         const currentStringChar = string[i];
         if (currentStringChar === target[0]) {
-            for (let x = 1; x < target.length; x++) {
-                const currentTargetChar = target[x];
-                if (currentTargetChar == string[i + x]) {
-                    if (x == target.length - 1) occurrences++;
+            if (target.length == 1) occurrences++;
+            else {
+                for (let x = 1; x < target.length; x++) {
+                    const currentTargetChar = target[x];
+                    if (currentTargetChar == string[i + x]) {
+                        if (x == target.length - 1) occurrences++;
+                    }
+                    else break;
                 }
-                else break;
             }
         }
     }
-    return  occurrences;
+    return occurrences;
 }
 
 
-console.log(naiveStringSearch("alealejandroal3", "ale"));
+console.log(naiveStringSearch("alealejandro", "ale"));
